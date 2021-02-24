0ff82711f9f902b6057c7ba804cd876ea3a389cc
Paolo Reyes <paolo.reyes.2911@gmail.com>
2021-02-24 15:45:43 -0500

    updates packages

--- web/src/client/App.js
+++ web/src/client/App.js
@@ -1,48 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { renderRoutes } from 'react-router-config';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-// const App2 = (props) => {
-// 	const [hide, setHide] = useState(props.isHided);
-// 	// const [items, setItems] = useState([]);
-// 	// useEffect(() => {
-// 	//     (async () => {
-// 	//         const response = await fetch('http://localhost:4000/items');
-// 	//         const newItems = await response.json();
-
-// 	//         setItems(newItems);
-// 	//     })();
-// 	// }, []);
-
-// 	const handleClick = () => {
-// 		setHide(!hide);
-// 	};
-
-// 	return (
-// 		<div>
-// 			<h1>Hola mundo</h1>
-// 			<button onClick={handleClick}>Toggle</button>
-// 			<ul>{!hide && props.items.map((item) => <li key={item.id}>{item.label}</li>)}</ul>
-// 		</div>
-// 	);
-// };
-
-// App.defaultProps = {
-// 	items: [
-// 		{ id: 1, label: 'test' },
-// 		{ id: 1, label: 'test2' },
-// 	],
-// };
-
-// const mapStateToProps = (state) => ({
-// 	isHided: state.toggle.isHided,
-// 	items: state.users,
-// });
-
-// export const App2 = {
-// 	component: connect(mapStateToProps, {})(App2),
-// };
 
 const App = ({ route }) => {
 	return <div>{renderRoutes(route.routes)}</div>;
--- web/src/server/bootstrap.js
+++ web/src/server/bootstrap.js
@@ -6,10 +6,6 @@ import routes from './routes';
 
 const app = express();
 
-// View engine setup
-app.set('views', path.join(__dirname, 'static', 'views'));
-app.set('view engine', 'ejs');
-
 // Basic express server configuration
 app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ extended: false }));
