f4edf0039ceaa7ea607fc838e555f11675504464
Paolo Reyes <paolo.reyes.2911@gmail.com>
2021-02-27 11:49:11 -0500

    helmet

--- web/src/server/bootstrap.js
+++ web/src/server/bootstrap.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import path from 'path';
 
+import { errorHandler } from './middlewares';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 // Basic express server configuration
 app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 app.use(cors());
 
 app.use('/public', express.static(path.join(__dirname, '../../dist/public')));
@@ -16,6 +19,9 @@ app.use('/assets', express.static(path.join(__dirname, './static/public')));
 
 // Load routes
 app.use(routes);
+
+// error handler
+router.use(errorHandler);
 
 // NodeJS/express: Cache and 304 status code
 app.disable('etag');
--- web/src/server/routes/index.js
+++ web/src/server/routes/index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import chatRoute from './chat';
 import notFoundRoute from './notFound';
 import welcomeRoute from './welcome';
-import { errorHandler } from './middlewares';
 import wrapAsyncHandler from '../utils/wrapAsyncHandler';
 
 const router = express.Router();
@@ -19,6 +18,5 @@ router.all('/chat-app/:chatId', wrapAsyncHandler(chatRoute));
 router.all('/welcome', wrapAsyncHandler(welcomeRoute));
 
 router.use('*', wrapAsyncHandler(notFoundRoute));
-router.use(errorHandler);
 
 export default router;
